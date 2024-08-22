1fce48519e5c33cb71368d98791fafca3e00648e
Liam Liangsu <only1turtle21@gmail.com>
2024-08-22 16:47:32 -0700

    Initial commit

---
+++ project.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class project {
+    // This is a program that ask the user for their weight and height
+    // and uses them to calculate the BMI
+    public static void main(String[] args) {
+        // initializing scanner 
+        Scanner scnr = new Scanner(System.in);
+        // initializing the variables
+
+        double weight;
+        double height;
+        double BMI;
+
+        // print question and get answers
+        System.out.println("Whhat is your weight (pounds) and height (inches)?");
+        weight = scnr.nextDouble();
+        height = scnr.nextDouble();
+
+        // Calculate BMI
+        BMI = (weight*703) / Math.pow(height, 2);
+        
+
+        // Print their BMI
+        System.out.println("Your BMI is not " + BMI + " percent ");
+    }
+}
